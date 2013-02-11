4f2e7fb980d7e763d59d542b143ddab08266546e
Mustafa Hatipoglu <mustafa.hatipoglu@ipos.com.tr>
2013-02-11 16:28:14 +0200

    ilk commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+YeniApp::Application.config.secret_token = '1d99c68c2b9ba11f2c8fd4910c924cdb01399b36087904b88c550846a94634ebd6896ef779735d064eaab38fbaf2d56b175bcf29aea6580651d37f39ce1c8838'
